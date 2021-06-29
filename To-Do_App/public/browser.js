e34f9260b3b7a67cb6d31d8d54bdf48a12adcd40
shalithadeshan <shalithad8520jayasekara@gmail.com>
2021-06-29 20:02:18 +0530

    create new item without reload and use client side rendering

--- To-Do_App/public/browser.js
+++ To-Do_App/public/browser.js
@@ -1,3 +1,35 @@
+function itemTemplate(item) {
+    return `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
+    <span class="item-text">${item.text}</span>
+    <div>
+      <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+      <button data-id="${item._id}" class="delete-me btn btn-danger btn-sm">Delete</button>
+    </div>
+  </li>`
+}
+
+// initial page load render
+let ourHtml = items.map(function(items) {
+    return itemTemplate(items)
+}).join('')
+document.getElementById("item-list").insertAdjacentHTML("beforeend", ourHtml)
+
+
+// create feature
+let createField = document.getElementById("create-field")
+
+document.getElementById("create-form").addEventListener("submit", function(e) {
+    e.preventDefault()
+    axios.post('/create-item', { text: createField.value }).then(function(response) {
+        document.getElementById("item-list").insertAdjacentHTML("beforeend", itemTemplate(response.data))
+        createField.value = ""
+        createField.focus()
+    }).catch(function() {
+        console.log("Try again later")
+    })
+})
+
+
 document.addEventListener("click", function(e) {
     // delete
     if (e.target.classList.contains("delete-me")) {
--- To-Do_App/server.js
+++ To-Do_App/server.js
@@ -16,8 +16,8 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.get('/', function(req, res) {
-            db.collection('items').find().toArray(function(err, items) {
-                        res.send(`<!DOCTYPE html>
+    db.collection('items').find().toArray(function(err, items) {
+        res.send(`<!DOCTYPE html>
 <html>
 <head>
   <meta charset="UTF-8">
@@ -30,49 +30,43 @@ app.get('/', function(req, res) {
     <h1 class="display-4 text-center py-1">To-Do App</h1>
     
     <div class="jumbotron p-3 shadow-sm">
-      <form action="/create-item" method="POST">
+      <form id="create-form" action="/create-item" method="POST">
         <div class="d-flex align-items-center">
-          <input name="item" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
+          <input id="create-field" name="item" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
           <button class="btn btn-primary">Add New Item</button>
         </div>
       </form>
     </div>
     
-    <ul class="list-group pb-5">
-    ${items.map(function(item) {
-      return `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-      <span class="item-text">${item.text}</span>
-      <div>
-        <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-        <button data-id="${item._id}" class="delete-me btn btn-danger btn-sm">Delete</button>
-      </div>
-    </li>`
-    }).join('')}
+    <ul id="item-list" class="list-group pb-5">
     </ul>
     
   </div>
 
+  <script>
+  let items = ${JSON.stringify(items)}
+  </script>
   <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
   <script src="/browser.js"></script>
 </body>
 </html>`)
-  })
+    })
 })
 
 app.post('/create-item', function(req, res) {
-    db.collection('items').insertOne({ text: req.body.item }, function() {
-        res.redirect('/')
+    db.collection('items').insertOne({ text: req.body.text }, function(err, info) {
+        res.json(info.ops[0])
     })
 })
 
 app.post('/update-item', function(req, res) {
-  db.collection('items').findOneAndUpdate({_id: new mongodb.ObjectId(req.body.id)}, {$set: {text: req.body.text}}, function() {
-    res.send("Success")
-  })
+    db.collection('items').findOneAndUpdate({ _id: new mongodb.ObjectId(req.body.id) }, { $set: { text: req.body.text } }, function() {
+        res.send("Success")
+    })
 })
 
-app.post('/delete-item', function(req,  res) {
-  db.collection('items').deleteOne({_id: new mongodb.ObjectId(req.body.id)}, function() {
-    res.send("Success")
-  })
+app.post('/delete-item', function(req, res) {
+    db.collection('items').deleteOne({ _id: new mongodb.ObjectId(req.body.id) }, function() {
+        res.send("Success")
+    })
 })
