bf8f19943183fcc33bf0dd93939c8c100366ed78
shalithadeshan <shalithad8520jayasekara@gmail.com>
2021-06-30 22:06:23 +0530

    add security to app

--- To-Do_App/server.js
+++ To-Do_App/server.js
@@ -1,5 +1,6 @@
 let express = require('express')
 let mongodb = require('mongodb')
+let sanitizeHTML = require('sanitize-html')
 
 let app = express()
 let db
@@ -15,6 +16,18 @@ mongodb.connect(connectionString, { useNewUrlParser: true }, function(err, clien
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+function passwordProtected(req, res, next) {
+    res.set('WWW-Authenticate', 'Basic realm="Simple todo App"')
+    console.log(req.headers.authorization)
+    if (req.headers.authorization == "Basic c2hhbGl0aGE6OTY2MDE4") {
+        next()
+    } else {
+        res.status(401).send("Authentication reqired")
+    }
+}
+
+app.use(passwordProtected)
+
 app.get('/', function(req, res) {
     db.collection('items').find().toArray(function(err, items) {
         res.send(`<!DOCTYPE html>
@@ -23,7 +36,8 @@ app.get('/', function(req, res) {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Simple To-Do App</title>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
+   integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
 </head>
 <body>
   <div class="container">
@@ -54,13 +68,15 @@ app.get('/', function(req, res) {
 })
 
 app.post('/create-item', function(req, res) {
-    db.collection('items').insertOne({ text: req.body.text }, function(err, info) {
+    let safeText = sanitizeHTML(req.body.text, { allowedTags: [], allowedAttributes: {} })
+    db.collection('items').insertOne({ text: safeText }, function(err, info) {
         res.json(info.ops[0])
     })
 })
 
 app.post('/update-item', function(req, res) {
-    db.collection('items').findOneAndUpdate({ _id: new mongodb.ObjectId(req.body.id) }, { $set: { text: req.body.text } }, function() {
+    let safeText = sanitizeHTML(req.body.text, { allowedTags: [], allowedAttributes: {} })
+    db.collection('items').findOneAndUpdate({ _id: new mongodb.ObjectId(req.body.id) }, { $set: { text: safeText } }, function() {
         res.send("Success")
     })
 })
